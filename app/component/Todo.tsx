f138c203bda8c7be4440b5d0e6e1c3a260d4cf93
csjames <csjames0303@gmail.com>
2023-07-25 00:52:45 +0800

    checkbox min width

--- app/component/Todo.tsx
+++ app/component/Todo.tsx
@@ -21,12 +21,12 @@ function Todo({ props }: Props) {
         deleteTodoAction(props.id.toString())
     }
   return (
-    <div className='py-2 w-screen lg:w-3/6 px-20 flex justify-center items-center'>
-           <div className='w-2/3'>
+    <div className='py-2 w-screen lg:w-3/6 px-5 ml-5  md:px-20 flex justify-center items-center'>
+           <div className='w-1/2 md:w-2/3'>
            <p className={`truncate text-sm lg:text-base w-full text-teal-200 ${props.isCompleted?('line-through'):(' ')}`}>{props.title}</p>
            </div>
-           <div className='w-1/3   px-10 grid grid-cols-2 min-w-1/3'>
-              <input onChange={handleChange} type="checkbox" name="" id="" defaultChecked={props.isCompleted} />
+           <div className='w-3/6 md:w-1/3   px-10 grid grid-cols-2 min-w-1/3'>
+              <input className='w-10' onChange={handleChange} type="checkbox" name="" id="" defaultChecked={props.isCompleted} />
               <div onClick={handleDelete} className='text-xs  min-w-min cursor-pointer col-span-1 p-2 border border-red-500 text-red-500 flex justify-center items-center'>
                   Remove
               </div>
